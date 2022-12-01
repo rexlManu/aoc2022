571995bef3e0068275c5ad27e51d5269f0082dde
Emmanuel Lampe <mail@emmanuel-lampe.de>
2022-12-01 16:56:50 +0100

    feat: part 2 of day 1

--- src/day1/index.js
+++ src/day1/index.js
@@ -5,6 +5,8 @@ let caloriesInput = read(new URL('./input', import.meta.url));
 let elves = caloriesInput.split(/\r?\n\r?\n/)
     .map(group => group.split(/\r?\n/).map(Number).reduce((a, b) => a + b, 0));
 
-let highestElve = elves.reduce((a, b) => a > b ? a : b, 0);
+let sortedElves = elves.sort((a, b) => a - b);
 
-console.log(highestElve);
+console.log("Highest number of calories:", sortedElves[sortedElves.length - 1]);
+
+console.log("Top 3:", sortedElves.slice(sortedElves.length - 3).reduce((a, b) => a + b, 0));
